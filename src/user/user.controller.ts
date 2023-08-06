644ad9d2a1001166245e2566466d6604d245db74
Felipe <felipe@easyteam.rocks>
2023-08-06 17:00:33 -0300

    feat: create returnUser Dto

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,18 @@
 import { UserService } from './user.service';
 import { CreateUserDto } from './dtos/createUser.dto';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UserEntity } from './interfaces/user.entity';
+import { ReturnUserDto } from './dtos/returnUser.dto';
 
 @Controller('user')
 export class UserController {
 
     constructor(private readonly userService: UserService) {}
 
+    @UsePipes(ValidationPipe)
     @Get()
-    async getAllUsers(): Promise<UserEntity[]> {
-        return this.userService.getAllUser();
+    async getAllUsers(): Promise<ReturnUserDto[]> {
+        return (await this.userService.getAllUser()).map((UserEntity) => new ReturnUserDto(UserEntity));
     }
 
     @Post()
